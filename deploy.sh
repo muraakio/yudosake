a4e33e5448f07c9a0f6c00f5e897d61286ad941f
chma <murasakio.korudo@gmail.com>
2020-12-07 19:33:06 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # set -e # stop on error
 
 npm run build
-cp ./source/CNAME ../dist
+cp source/CNAME dist/
 git add .
 git commit -m 'deploy to gh-pages'
 git subtree push --prefix dist origin gh-pages
